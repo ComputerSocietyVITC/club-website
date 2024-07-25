5ff8b2724409ad855bb757d8b29641971b3a4cd5
Aayush Shukla <oreus.aks@gmail.com>
2024-07-25 22:52:11 +0530

    Completed the team page without framer motion for now

---
+++ src/data/teamdata.js
@@ -0,0 +1,100 @@
+const image = "/people/shashwat.webp"
+const image2 = "/people/jasmine.webp"
+
+const team = [
+  {
+    name: "Harshit Kumar Singh",
+    designation: "President",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQFRwISnjTYSeQ/profile-displayphoto-shrink_800_800/0/1719988715088?e=1726704000&v=beta&t=3IMFNPh2hMf6k1oA2fA8mzbzj78w25C4tZeqtG5Qvcw",
+    linkUrl: "https://www.linkedin.com/in/harshitkumarsingh25/",
+    gitUrl: "https://github.com/harshitsinghcode",
+  },
+  {
+    name: "Mohammed Faiz Abid",
+    designation: "Vice-Chairperson",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQE4o90wdmTv7Q/profile-displayphoto-shrink_800_800/0/1692941127887?e=1724284800&v=beta&t=AnqpIKc322AO9Wbj95cFu-7rtK78U-2Yk0pIfH5CxP0",
+    linkUrl: "https://www.linkedin.com/in/mohammad-faiz-abid-875371194/",
+    gitUrl: "https://github.com/atfaizzz",
+  },
+  {
+    name: "Anvesha Singh",
+    designation: "General Secretary",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQHFwoa3ehyK5Q/profile-displayphoto-shrink_800_800/0/1692123083098?e=1724284800&v=beta&t=PkVbQmZYLJ7WAPiDHH3yfAAvXzg880JsrJWUNOpwWhE",
+    linkUrl: "https://www.linkedin.com/in/anvesha-singh-330ba9260/",
+    gitUrl: "https://github.com/Anvesha-Singh",
+  },
+  {
+    name: "Sandeep Ravisankar",
+    designation: "Treasurer",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQEEENE9sTgaUw/profile-displayphoto-shrink_800_800/0/1707332074112?e=1724284800&v=beta&t=GFPcQ4H6B7lr5N1c1crfdIJZA6OR2Rvhfe4ZUK483-0",
+    linkUrl: "https://www.linkedin.com/in/sandeep-ravisankar-9113a5271/",
+    gitUrl: "https://github.com/sandman-04",
+  },
+  {
+    name: "Anirudh Sridhar",
+    designation: "HR and Management Head",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D4E03AQEsnRZvlmFENQ/profile-displayphoto-shrink_800_800/0/1698810602543?e=1724284800&v=beta&t=1Dvx4wvFVQQaL8TvLEU5MuHCs_cE4Lo-0Slzfj3dQFY",
+    linkUrl: "https://www.linkedin.com/in/anirudh-sridhar-07bb02298/",
+    gitUrl: "http://github.com/shotoyaar",
+  },
+  {
+    name: "Teeshta Parmar",
+    designation: "Social Media and Content Head",
+    imageUrl: "https://media.licdn.com/dms/image/D4D03AQHbn_M8U-mkOA/profile-displayphoto-shrink_800_800/0/1718774973835?e=1724889600&v=beta&t=FXURKbokpWgo08hsn8NW2VZ0lwqrBEGduENJEL6m9vA",
+    linkUrl: "",
+    gitUrl: "",
+  },
+  {
+    name: "Mrithulasree Nainar",
+    designation: "Design Head",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQHlYju7WtSwhg/profile-displayphoto-shrink_800_800/0/1686661431971?e=1724284800&v=beta&t=yVjLNKowejU78A9_GEgBuCbC5RBe6VH6IJb_K-gMDHQ",
+    linkUrl: "https://www.linkedin.com/in/mrithulasree-nainar-05a1b8271/",
+    gitUrl: "",
+  },
+  {
+    name: "Jasmine Tresa Jose",
+    designation: "UI/UX Head",
+    imageUrl: image2,
+    linkUrl: "https://www.linkedin.com/in/jasmine-tresa-jose-488385252/",
+    gitUrl: "",
+  },
+  {
+    name: "Sam Shervin S",
+    designation: "Technical Head",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQG2WlXjtdwlAQ/profile-displayphoto-shrink_800_800/0/1672170455042?e=1724284800&v=beta&t=27XtDouHCgWzpN7KS5k7L2kxLEEw591QAFfK1yisQfk",
+    linkUrl: "https://www.linkedin.com/in/sam-shervin/",
+    gitUrl: "https://github.com/sam-shervin",
+  },
+  {
+    name: "Shashwat Mishra",
+    designation: "Projects Head",
+    imageUrl: image,
+    linkUrl: "",
+    gitUrl: "https://github.com/Shashwatm74",
+  },
+  {
+    name: "Aditya Jyoti",
+    designation: "Joint General Secretary",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D4D03AQH-nsGX5TLQYg/profile-displayphoto-shrink_800_800/0/1696693366681?e=1724284800&v=beta&t=krCxXJAOhRAUO7vM9GKzc7c548opOaDrk4apZUfBg9A",
+    linkUrl: "https://www.linkedin.com/in/aditya-jyoti/",
+    gitUrl: "https://github.com/Aditya-Jyoti",
+  },
+  {
+    name: "Reuben Philip",
+    designation: "Joint Technical Head",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D5603AQHPkJ-hQsxJcg/profile-displayphoto-shrink_800_800/0/1710059683582?e=1724284800&v=beta&t=oG8B5a31t7A4uohY4k5EMKXzSXjunTAa8mBT-37bPqM",
+    linkUrl: "https://www.linkedin.com/in/reuben-philip-a96b67284/",
+    gitUrl: "https://github.com/AltSumpreme",
+  },
+];
+
+export default team;
